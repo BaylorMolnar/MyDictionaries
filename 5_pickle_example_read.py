3648c389abd5d453cb776bf7aa589f5ee990c34c
Baylor Molnar <baylor_molnar1@baylor.edu>
2021-02-08 15:14:14 -0600

    pickle stuff

---
+++ 5_pickle_example_read.py
@@ -0,0 +1,20 @@
+import pickle
+
+infile = open("names_pickle_file_write.dat", "rb")
+
+
+names = pickle.load(infile)
+
+print(type(names))
+
+print(names)
+
+name = input("Add a name to the list: ")
+names.append(name)
+
+print(names)
+outfile = open("names_pickle_file_write.dat", "wb")
+pickle.dump(names, outfile)
+
+infile.close()
+outfile.close()
